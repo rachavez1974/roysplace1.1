13a13039a3726feac4339c010a92e74d9cff8309
Ramon Chavez <rachavez1974@gmail.com>
2018-09-04 13:06:11 -0400

    add a funciton helper to provide titles for pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	#Returns a full title if one not provided
+	def full_title(page_title = '')
+
+		base_title = "Roy's Place, a Sandwhich Heaven"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
