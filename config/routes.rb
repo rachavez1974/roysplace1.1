0a71ac98123c3ce1ede8dc9dad1b25ee5fbaf1fc
Ramon Chavez <rachavez1974@gmail.com>
2018-09-04 00:30:15 -0400

    Finish static pages for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
   get 'static_pages/menus'
 
@@ -10,8 +10,5 @@ Rails.application.routes.draw do
   get 'static_pages/offers'
 
   get 'static_pages/bagged'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root 'application#hello'
+  
 end
